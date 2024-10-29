31f97e515957cedee6a35c5ddf8d5372ddcba322
Natasha-n-j <natashanjkaranja@gmail.com>
2024-10-29 17:19:01 +0300

    added js 5

---
+++ odd-or-even.js
@@ -0,0 +1,9 @@
+number = parseInt(prompt('Enter number: '));
+
+if(number % 2) {
+  alert("This number is an even number: " + number)
+}
+
+else {
+  alert("This number is an odd number: " + number)
+}
